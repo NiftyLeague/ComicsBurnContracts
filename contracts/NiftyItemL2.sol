b83bf952feba8abf2ad72022ac72659a31ea4f4f
0xKey <davidlee152025@gmail.com>
2022-11-17 21:48:19 +0900

    update tokenURI with tokenID

--- contracts/NiftyItemL2.sol
+++ contracts/NiftyItemL2.sol
@@ -71,13 +71,6 @@ contract NiftyItemL2 is ERC721EnumerableUpgradeable, OwnableUpgradeable, Pausabl
     return uri;
   }
 
-  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
-    string memory baseURI = _baseURI();
-    return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, itemIdByTokenId[tokenId].toString())) : "";
-  }
-
   function burn(uint256 _tokenId) external whenNotPaused {
     _burn(_tokenId);
   }
